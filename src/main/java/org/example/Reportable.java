44f7a7c027071c51b9b3092e76ab9cd3e01e6b9b
6311236 <6311236@edu.vaniercollege.qc.ca>
2026-05-13 23:40:16 -0400

    Add reportable interface for the inventiry text reports

---
+++ src/main/java/org/example/Reportable.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface Reportable {
+
+    /**
+     * will be used to make a report that describes the items hrouped by borrowing status'
+     * @return the report
+     */
+    String generateInventoryReport();
+}
