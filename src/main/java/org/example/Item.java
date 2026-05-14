c92854e9172bc55c8279ae088bb639cef5bae08e
6311236 <6311236@edu.vaniercollege.qc.ca>
2026-05-14 00:44:01 -0400

    Create the abstract item class with it's data fields and allocateNextItemId method, seedItemIdSequence methoid and the extractSequence method

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+public abstract class Item {
+    private String id;
+    private String title;
+    private ItemStatus status;
+
+    private static int nextId = 1;
+
+    /**
+     * Allocates the next Id
+     * @return the id formated ex: I-00001
+     */
+    public static String allocateNextItemId() {
+        return String.format("I-%05d", nextId++); // Better than having the usual %04d which is ionly ex: 0001
+    }
+
+    /**
+     * Moves the id gen. forward after a csv
+     * @param nextExclusive the lowest value that shouldnt override current id
+     */
+    public static void seedItemIdSequence(int nextExclusive) {
+        if (nextExclusive > nextId) {
+            nextId = nextExclusive;
+        }
+    }
+
+    /**
+     * Reads numeral of ids to sequence
+     * @param id the input id
+     * @return the numerical suffix and if not will be 0 if not formed properly
+     */
+    public static int extractSequence(String id) {
+        if (id == null || !id.startsWith("I-")) {
+            return 0;
+        }
+        return Integer.parseInt(id.substring(2));
+    }
+}
