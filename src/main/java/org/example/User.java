a7dce2677ee1658bc306db6df832c1e11db2117d
6311236 <6311236@edu.vaniercollege.qc.ca>
2026-05-14 03:58:07 -0400

    Add the getBorrowedItems, canBorrow and register/unregister borrow methods. Also created Admin, Student and Teacher skeleton classes to allow creation of canBorrow method.

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -31,7 +32,7 @@ public abstract class User {
      * @return the nextId
      */
     public static String allocateNextId() {
-        return String.format("U-%04d", nextId++);
+        return String.format("U-%04d", nextId++); // User variant of the nextId with "U" prefix
     }
 
     /**
@@ -54,5 +55,58 @@ public abstract class User {
             return 0;
         }
         return Integer.parseInt(id.substring(2));
+    }
+
+    /**
+     * Gets list of active borrowed items
+     * @return the same list not modified
+     */
+    public List<Item> getBorrowedItems() {
+        return Collections.unmodifiableList(borrowedItems); // Only want to view it so not gonna modify it
+    }
+
+    /**
+     * Counts how many borrowed books user has
+     * @return the number of boroweed books
+     */
+    public long countBorrowedBooks() {
+        return borrowedItems.stream().filter(Book.class::isInstance).count();
+    }
+
+    /**
+     * used to apply the limits that different types of users can borrow at a time
+     * @param candidate the item that the user wants to borrow
+     * @return will return true if < to the limit (and so allow the borrowing)
+     */
+    public boolean canBorrow(Item candidate) {
+        if (this instanceof Student) {
+            if (candidate instanceof Book) {
+                return countBorrowedBooks() < Constants.MAX_BOOKS_STUDENT;
+            }
+            return true;
+        }
+        if (this instanceof Teacher) {
+            return borrowedItems.size() < Constants.MAX_ITEMS_TEACHER;
+        }
+        if (this instanceof Admin) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * adds the input item to borrowedItems
+     * @param item the input item
+     */
+    void registerBorrow(Item item) {
+        borrowedItems.add(item);
+    }
+
+    /**
+     * removes the input item from borrowedItems
+     * @param item the input item
+     */
+    void unregisterBorrow(Item item) {
+        borrowedItems.remove(item);
     }
 }
